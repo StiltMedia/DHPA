58d930f3c867fc2abe093fc1ac0f2daf51be1e26
Mariano Ponce <marianojponce@gmail.com>
2015-12-01 21:11:23 +0800

    Update screen dimension
    Change design dimensions from 1920px to 1280px

--- form.php
+++
@@ -1,7 +0,0 @@
-<form>
-	<input type="text" placeholder="Name">
-	<input type="text" placeholder="Email">
-	<input type="text" placeholder="Phone">
-	<input type="text" placeholder="Appointment Time">
-	<input type="submit" value="Book it!">
-</form>
